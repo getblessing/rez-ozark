9c96e357323d65327ebebe0379ac44813488d11d
David Lai <davidlatwe@gmail.com>
2020-11-04 14:51:48 +0800

    update config

--- config/allzparkconfig.py
+++ config/allzparkconfig.py
@@ -48,11 +48,51 @@ def applications():
     return []
 
 
-def style_loader():
+def themes():
+    _themes = []
+
     try:
         from ozark import style
     except ImportError:
         print("Failed to load Ozark CSS stylesheet.")
-        return
+    else:
+        _themes.append({
+            "name": "avalon",
+            "source": style.load_stylesheet(),
+        })
 
-    return style.load_stylesheet()
+    return _themes
+
+
+def application_parent_environment():
+    import os
+    from rez.resolved_context import ResolvedContext
+
+    rxt = os.getenv("REZ_RXT_FILE")
+    if not rxt:
+        return os.environ.copy()
+
+    # Allzpark is launched from a Rez resolved context, need to compute the
+    # original environment so the application can be launched in a proper
+    # parent environment.
+
+    # TODO: Load environment from "house" package (.env file)
+
+    context = ResolvedContext.load(rxt)
+    context.append_sys_path = False
+    changes = context.get_environ()
+    current = os.environ.copy()
+    rollbacked = dict()
+
+    for key, value in current.items():
+        current_paths = value.split(os.pathsep)
+        changed_paths = changes.get(key, "").split(os.pathsep)
+        roll_paths = []
+        for path in current_paths:
+            if path and path not in changed_paths:
+                roll_paths.append(path)
+
+        if roll_paths:
+            rollbacked[key] = os.pathsep.join(roll_paths)
+
+    return rollbacked
